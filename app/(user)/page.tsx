b442bee88974c95d1dc0e17f8377dd8a991701ed
sanjayguwaju <sanjayguwaju@gmail.com>
2023-03-10 18:12:51 +0545

    feat: preview mode transition

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,25 @@
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+
+const query = groq`
+*[_type=='post'] {
+    ...,
+    author->,
+    categories[]->
+    | order(_createdAt desc)
+}
+`
+
 export default function Homepage() {
+    if(previewData()) {
+        return (
+            <div>Preview mode</div>
+        )
+    }
+
     return (
         <div>
-            <h1 className="text-4xl">Welcome to the Blog</h1>
+            <h1>Not in Preview mode</h1>
         </div>
     )
 }
